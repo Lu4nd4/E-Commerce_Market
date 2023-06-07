017322efdf5d083c262a1737daa3f1bd7c1ec4e2
Luanda Nuredini <74864261+Lu4nd4@users.noreply.github.com>
2023-06-07 14:43:29 +0200

    Update stocks.js

--- src/components/stocks/stocks.js
+++ src/components/stocks/stocks.js
@@ -159,14 +159,14 @@ const linkClicked = ()=>{
       <div className="stocks">
         {/* <img src={myimage} alt="" className="monitor" /> */}
         <Link to={"/description/" + props.id}>
-          <svg
+           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="300"
             height="200"
             fill="currentColor"
             className="bi bi-image-fill monitor"
             viewBox="0 0 16 16"
-            onClick={linkClicked}
+            onClick={()=> props.removed(true)}
           >
             <path d="M.002 3a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-12a2 2 0 0 1-2-2V3zm1 9v1a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V9.5l-3.777-1.947a.5.5 0 0 0-.577.093l-3.71 3.71-2.66-1.772a.5.5 0 0 0-.63.062L1.002 12zm5-6.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0z" />
           </svg>
